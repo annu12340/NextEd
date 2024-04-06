54688f1dba9554b2b752f2da801a437340461fe9
annu12340 <annujolly17@gmail.com>
2024-04-07 00:01:24 +0530

    Working on virtual pet

--- virtualpet/models.py
+++ virtualpet/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    score = models.IntegerField()
+    owner = models.IntegerField()
